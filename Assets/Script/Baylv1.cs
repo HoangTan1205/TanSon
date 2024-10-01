daa1ccb131e3c457a0b5c5692020fa477e99666f
HoangTan1205 <tanhvph53409@gmail.com>
2024-10-01 13:10:23 -0700

    demo_lv1

---
+++ Assets/Script/Baylv1.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baylv1 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public bool cham = false;
+    public float tocDo = 3f;
+    float moveInput = 1f;
+    float moveSpeed = 50f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+        if (cham)
+        {
+            
+            rb.velocity = new Vector2( moveSpeed * Time.deltaTime, rb.velocity.y);
+
+
+            if (transform.position.x < 10f) 
+            {
+                Destroy(gameObject); 
+            }
+
+        }
+
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            cham = true;
+        }
+    }
+
+
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 10f; // Tốc độ di chuyển
+    public float jumpForce = 10f; // Lực nhảy
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Flip the character if moving left or right
+        if (moveInput > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (moveInput < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Kiểm tra xem nhân vật có tiếp xúc với mặt đất hay không
+        if (collision.gameObject.CompareTag("Nen"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Khi rời mặt đất
+        if (collision.gameObject.CompareTag("Nen"))
+        {
+            isGrounded = false;
+        }
+    }
+}
