623b56dd47f4878ed49595a748a88d53501e277e
HoangTan1205 <tanhvph53409@gmail.com>
2024-10-20 18:00:30 +0700

    sua_tocdo

--- Assets/Script/Trap/TrapLv3.cs
+++ Assets/Script/Trap/TrapLv3.cs
@@ -20,7 +20,7 @@ public class TrapLv3 : MonoBehaviour
     {
         yield return new WaitForSeconds(2f);
         rb.velocity = new Vector2(moveSpeed * Time.deltaTime, rb.velocity.y);
-        yield return new WaitForSeconds(2.3f);
+        yield return new WaitForSeconds(2.1f);
         moveSpeed = 0;
     }
 
